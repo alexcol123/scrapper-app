13e33830156b92930773a274010478cd196d5c1e
Henry Munoz <alexcol123@yahoo.com>
2024-02-21 11:16:38 -0500

    fixed errorns on api cron route 2

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -4,7 +4,7 @@ import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { scrapedAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
